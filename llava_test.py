f60aac87fcd90c41e1798cacfffe9d7b4b7aaaf5
trainer_brendan <omkarpast@gmail.com>
2025-02-01 19:32:59 +0530

    Update llava_test.py

---
+++ llava_test.py
@@ -0,0 +1,34 @@
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(model="llava", temperature=0)
+
+
+def prompt_func(data):
+    text = data["text"]
+    image = data["image"]
+
+    image_part = {
+        "type": "image_url",
+        "image_url": f"data:image/jpeg;base64,{image}",
+    }
+
+    content_parts = []
+
+    text_part = {"type": "text", "text": text}
+
+    content_parts.append(image_part)
+    content_parts.append(text_part)
+
+    return [HumanMessage(content=content_parts)]
+
+
+from langchain_core.output_parsers import StrOutputParser
+
+chain = prompt_func | llm | StrOutputParser()
+
+query_chain = chain.invoke(
+    {"text": "What is the Dollar-based gross retention rate?", "image": image_b64}
+)
+
+print(query_chain)
